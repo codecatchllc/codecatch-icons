1de1ba81942f97ea61ed9b2e912193beb6d305f2
Cameron Shatto <cameronshatto99@gmail.com>
2022-03-06 18:51:28 -0600

    fix: attempt to fix security issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,27 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 const packageJson = require("./package.json");
 
-export default {
-  // entry points
-  input: "./src/index.ts",
-  // output files
-  output: [
-    {
-      file: packageJson.module,
-      format: "esm", // ES Modules
-      sourcemap: true,
-    },
-  ],
-  // plugins array
-  plugins: [
-    peerDepsExternal(), // prevents building peerDependencies
-    resolve(), // resolves package entrypoints
-    typescript({ useTsconfigDeclarationDir: true }), // typescript
-    uglify({ sourcemap: true }),
-  ],
-};
+export default [
+  {
+    // entry point
+    input: "./src/index.ts",
+    // output files
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm", // ES Modules
+        sourcemap: true,
+      },
+    ],
+    // plugins array
+    plugins: [
+      peerDepsExternal(), // prevents building peerDependencies
+      resolve(), // resolves package entrypoints
+      typescript({ useTsconfigDeclarationDir: true }), // typescript
+      terser(),
+    ],
+  },
+];
