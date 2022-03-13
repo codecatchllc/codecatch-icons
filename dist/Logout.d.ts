8591a5b683faa010369bc0cfbca386e9158364ca
Cameron Shatto <cameronshatto99@gmail.com>
2022-03-13 11:08:18 -0500

    fix(ISSUE-#2): modified the build process. Now using tsup and esbuild to build the source code

--- dist/Logout.d.ts
+++
@@ -1,9 +0,0 @@
-import { SVGProps } from "react";
-interface SVGRProps {
-    title?: string;
-    titleId?: string;
-    size?: string | number;
-    color?: string;
-}
-declare function LogoutIcon({ title, titleId, size, color, className, ...rest }: SVGProps<SVGSVGElement> & SVGRProps): JSX.Element;
-export default LogoutIcon;
