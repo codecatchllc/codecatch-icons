64eac6cf6c333a1939829a59bbb707989969ca68
AustinLeath <austinleath18@gmail.com>
2022-11-03 04:34:50 -0500

    fix(add-follow-unfollow-icons): added follow and unfollow icons

---
+++ src/Follow.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+  size?: string | number;
+  color?: string;
+}
+function FollowIcon({
+  title,
+  titleId,
+  size = 24,
+  color = "#F8F9FA",
+  className,
+  ...rest
+}: SVGProps<SVGSVGElement> & SVGRProps) {
+  const classNames = ["codecatch-Icon", className];
+  const props = {
+    ...{
+      role: "img",
+      alt: "Follow Icon",
+      "data-testid": "codecatch-FollowIcon",
+      width: size,
+      height: size,
+      className: classNames.join(" ").trim(),
+    },
+    ...rest,
+  };
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={color}
+      stroke="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 24 24"
+      aria-labelledby={titleId}
+      {...props}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <g stroke="none">
+        <path fill="none" d="M0 0h24v24H0z" />
+        <path d="M14 14.252v2.09A6 6 0 0 0 6 22l-2-.001a8 8 0 0 1 10-7.748zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm5.793 8.914 3.535-3.535 1.415 1.414-4.95 4.95-3.536-3.536 1.415-1.414 2.12 2.121z" />
+      </g>
+    </svg>
+  );
+}
+
+export default FollowIcon;
